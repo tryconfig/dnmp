4d3decabbb8bfc863a120b3bb0b6bf889dcd5dc3
Tryconfig <38839635+tryconfig@users.noreply.github.com>
2018-12-27 08:07:34 +0000

    Updated 20181227

--- ng_conf/rotate.sh
+++ ng_conf/rotate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 LOGS_PATH=
 YESTERDAY=$(date -d "yesterday" +%Y%m%d)
-ls -l ${LOGS_PATH}/*.log |grep -v 20|awk '{print $9}' >/tmp/rotate_tmp.txt
+ls -l ${LOGS_PATH}/*.log | awk '{print $9}' >/tmp/rotate_tmp.txt
 cat /tmp/rotate_tmp.txt | while read LINE
 do
         SIZE=$(ls -l ${LINE} | awk '{print $5}')
         test ${SIZE} -gt 0 && mv ${LINE} ${LINE}_${YESTERDAY}
 done
 rm -rf /tmp/rotate_tmp.txt
-docker exec dnmp_web_1 nginx -s reload
+docker exec dnmp_nginx nginx -s reload
